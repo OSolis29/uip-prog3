15344fc8149d4616f3a9b4a42b79bff144474bda
OSolis29 <osver9529z@gmail.com>
2015-08-24 20:20:03 -0500

    parcial 2
    disculpe Prof. no pude hacer exactamente el código pedido pero tiene la misma utilidad.

--- parcial 2/traductor.py
+++ parcial 2/traductor.py
@@ -11,7 +11,7 @@ class Proyecto(BoxLayout):
 	traduccion = ObjectProperty()
 
 	def traductor(self):
-		dicc = {'perro': 'dog', 'carro': 'car', 'agua': 'water'}
+		dicc = {"gato":"cat","lobo":"wolf","perro":"dog","zorro":"fox","pájaro":"bird","gallina":"hen","pato":"duck","rata":"rat","cordero":"lamb","serpiente":"snake","vaca":"cow","mariposa":"butterfly","caballo":"horse","mosquito":"mosquito","elefante":"elephant","ballena":"whale","araña":"spider","gallo":"cock","abeja":"bee","águila":"eagle","pollito":"chick","mono":"monkey","caracol":"snail","camello":"camel","pez":"fish","conejo":"rabbit","toro":"bull","chancho":"pig","jirafa":"giraffe","hormiga":"ant","tigre":"tiger","león":"lion","pantera":"panther","hornero":"baker","burro":"donkey","luciérnaga":"firefly","ganso":"goose","ardilla":"squirrel","canguro":"kangaroo","hipopótamo":"hippopotamus","insecto":"insect","rinoceronte":"rhinoceros","pulpo":"octopus","cebra":"zebra","langosta":"crab","cocodrilo":"crocodile","oso":"bear","cabra":"goat","pingüino":"penguin","coala":"koala","pavo":"turkey","sapo":"toad","pavo real":"peacock","rana":"frog","paloma":"pigeon or dove","cisne":"swan" }
 		for k,v in dicc.items():
 			if k == self.palabra.text:
 				self.traduccion.text = dicc[self.palabra.text]
